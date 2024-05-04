9ff422b7954e3d831ce586c7cccdc092ff7beac1
Laura Ducret <lauraducret@MacBook-Pro-de-Laura.local>
2024-05-04 14:32:16 +0800

    Initial Commit

---
+++ ExampleProject/ExampleProject/README.md
@@ -0,0 +1,9 @@
+//
+//  README.md
+//  ExampleProject
+//
+//  This read me is an example template
+Hello World!
+//
+
+import Foundation
---
+++ ExampleProject/ExampleProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ExampleProject
+//
+//  Created by Laura Ducret on 04.05.24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
